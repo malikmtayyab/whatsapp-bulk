850d356b4517a1d71b060dcbe41aa219bee81f50
malikmtayyab <tayyabshahid597@gmail.com>
2024-09-11 19:12:52 +0500

    Stringigy added

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -20,7 +20,7 @@ export async function GET(request: NextRequest) {
 
 export async function POST(request: NextRequest) {
   try {
-    console.log("requestBody", request);
+    console.log("requestBody", JSON.stringify(request));
 
     return NextResponse.json(
       {
