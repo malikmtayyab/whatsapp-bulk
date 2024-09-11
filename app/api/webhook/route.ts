6e3fa716c29bb05408424989e6b44be63086415e
malikmtayyab <tayyabshahid597@gmail.com>
2024-09-11 17:43:07 +0500

    request.json removed

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,9 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  const requestBody = await request.json();
-
-  console.log("requestBody", requestBody);
+  console.log("requestBody", request);
 
   try {
     return NextResponse.json(
