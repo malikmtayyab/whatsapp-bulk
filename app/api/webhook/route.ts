92941122d67fb158cbc614f827ea67971b52a016
malikmtayyab <tayyabshahid597@gmail.com>
2024-09-11 17:46:55 +0500

    JSON.stringify in request console

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  console.log("requestBody", request);
+  console.log("requestBody", JSON.stringify(request));
 
   try {
     return NextResponse.json(
